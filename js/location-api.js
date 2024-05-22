e237706eb4248d0b2942e9ca42f480eca292d796
Anders Ö <142984147+anders0b@users.noreply.github.com>
2024-05-22 14:07:26 +0200

    Update location-api.js

--- js/location-api.js
+++ js/location-api.js
@@ -3,7 +3,7 @@ var delay = 1000;
 
 async function getApi() {
   const apiUrl =
-    "https://ipgeolocation.abstractapi.com/v1/?api_key=046ef8b54aa440188af86cc0f158911d";
+    "https://ipgeolocation.abstractapi.com/v1/?api_key";
   const response = await fetch(apiUrl);
   const results = await response.json();
   locationText.innerText = `Vi fraktar till ${results.city}! *`;
